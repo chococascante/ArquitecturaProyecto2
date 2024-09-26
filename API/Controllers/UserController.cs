51825ef51b980b53b209e74091f354c972f48320
Luis Cascante <luisalonzocascante@gmail.com>
2024-09-26 17:32:26 -0600

    arquitectura inicial

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<User> _users = new List<User>();
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<User> Get()
+        {
+            // return users
+            return _users;
+
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post(User value)
+        {
+            Console.WriteLine($"{value.Email}");
+            _users.Add(value);
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
